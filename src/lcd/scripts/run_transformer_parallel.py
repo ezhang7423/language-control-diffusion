db2a2c2ef95d7463246725a2f0b704acb06a66d6
ezhang7423 <ezhang7423@student.palomar.edu>
2023-08-04 01:22:15 +0000

    add lr

--- src/lcd/scripts/run_transformer_parallel.py
+++ src/lcd/scripts/run_transformer_parallel.py
@@ -1,11 +1,12 @@
 import itertools
 import os
-
+import time
 
 seeds = [0, 12, 42]
-sizes = [(2048, 4), (4096, 6), (8192, 24)]
+sizes = [(4096, 4)]
+lr = [2e-4, 4e-4]
 
-
-for i, (seed, size) in enumerate(itertools.product(seeds, sizes)):
-    gpu_id = (i % 8) + 3
-    os.system(f'CUDA_VISIBLE_DEVICES={gpu_id} lcd train_transformer --seed {seed} --use-wandb --width {size[0]} --depth {size[1]} | tee {i}.log &')
+for i, (l, seed, size) in enumerate(itertools.product(lr, seeds, sizes)):
+    gpu_id = (i % 8)
+    os.system(f'CUDA_VISIBLE_DEVICES={gpu_id} lcd train_transformer --lr {l} --seed {seed} --use-wandb --width {size[0]} --depth {size[1]} > {i}.log 2>&1 &')
+    time.sleep(1.1)
