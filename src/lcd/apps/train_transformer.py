4e46bad75164202622238bac8c10e04f0810c0c9
ezhang7423 <ezhang7423@student.palomar.edu>
2023-11-13 04:35:31 -0800

    transformer large

--- src/lcd/apps/train_transformer.py
+++ src/lcd/apps/train_transformer.py
@@ -25,8 +25,11 @@ from lcd.utils.config import AttriDict
 from lcd.utils.eval import evaluate_policy, print_and_save
 from lcd.apps.rollout import set_state
 
-
-
+from accelerate import Accelerator
+from torch.distributed.elastic.multiprocessing.errors import record
+accelerator = Accelerator()
+# device = 'cuda'
+device = accelerator.device
 
 def wlog(*args, **kwargs):
     if wandb.run is not None:
@@ -42,6 +45,7 @@ class eval_args:
     skip_eval: bool = False
     num_sequences: int = 1000    
 
+@record
 def main(
     seed: int = 12,
     width: int = 4096,
@@ -52,7 +56,7 @@ def main(
     lr: float = 2e-5,
     use_wandb: bool = False,
     skip_eval: bool = False,
-):
+):    
     args, _, _, values = inspect.getargvalues(inspect.currentframe())
     args = AttriDict(
         {k: v for k, v in values.items() if k in args}
@@ -70,7 +74,7 @@ def main(
     exp_path = (
         DATA_PATH
         / "transformer-ablation"
-        / datetime.datetime.now().strftime("%m-%d_%H:%M:%S")
+        / datetime.datetime.now().strftime("%m-%d_%H:%M:%S.%f")
     )
     args.log_dir = exp_path
     exp_path.mkdir(parents=True)
@@ -118,6 +122,7 @@ def main(
             pin_memory=True,
         )
     )
+    
 
     eval_arg = eval_args(args, eval_state, model, skip_eval, 2)
     # validation evaluation is working
@@ -125,7 +130,11 @@ def main(
 
     # train
     optimizer = torch.optim.Adam(model.parameters(), lr=lr)
-    model.to("cuda")
+    model.to(device)
+    
+    model, optimizer, dataloader = accelerator.prepare(model, optimizer, dataloader)
+            
+    model.train()
     for epoch in range(num_epochs):
         print('*'*50)
         print(f'{epoch=}')
@@ -133,12 +142,13 @@ def main(
         torch.save(model, exp_path / f"model_{epoch}.pt")
         for i in tqdm.tqdm(range(num_steps)):
             batch: Batch = batch_to_device(next(dataloader)[0])
-            traj = batch.trajectories.flatten(end_dim=1)
+            traj = batch.trajectories.flatten(end_dim=1).to(device)
             pred = model.forward(
                 traj[:, 32:], batch.conditions.repeat_interleave(4, dim=0)
-            )
+            ).to(device)
             loss = ((pred - traj[:, :32]) ** 2).mean()
-            loss.backward()
+            accelerator.backward(loss)
+            # loss.backward()
             optimizer.step()
             optimizer.zero_grad()
             if use_wandb:
@@ -150,6 +160,7 @@ def main(
         if not (epoch % 5):
             eval_arg.num_sequences = 10
             evaluate(eval_arg)
+            
 
     eval_arg.num_sequences = 1000
     evaluate(eval_arg)
@@ -160,6 +171,7 @@ def main(
 
 def evaluate(eval_arg: eval_args):
     # run evaluation
+    eval_arg.model.eval()
     if eval_arg.skip_eval:
         return
 
@@ -167,7 +179,7 @@ def evaluate(eval_arg: eval_args):
     args.update(
         {
             "num_sequences": eval_arg.num_sequences,
-            "dm": TransformerEvaluationWrapper(eval_arg.model, device="cuda"),
+            "dm": TransformerEvaluationWrapper(eval_arg.model, device=device),
             "ep_len": 360,
             "subgoal_interval": 16,
         }
@@ -179,7 +191,16 @@ def evaluate(eval_arg: eval_args):
     eval_results = print_and_save(results, args, histories, model_id)
 
     wlog({f"eval/{k}": v for k, v in eval_results.items()})
+    eval_arg.model.train()
 
 
 if __name__ == "__main__":
-    main()
+    
+    main(
+        width=16384,
+        depth=24,
+        # batch_size=72,
+        # skip_eval=True,
+        # lr=2e-5,
+        use_wandb=True
+    )
